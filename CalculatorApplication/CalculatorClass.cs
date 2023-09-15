7c8d0e0f305f4bea58be98a87daff2a443bf0069
COMPUTER <COMPUTER@DESKTOP-BFLPBJI>
2023-09-15 23:11:26 +0800

    Add project files.

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate T Formula<T>(T arg1);
+    internal class CalculatorClass
+    {
+        public Formula<double> frmula;
+
+
+        public double GetSum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double GetDifference(double a, double b)
+        {
+            return a - b;
+        }
+
+        public event Formula<double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
+                frmula += value;
+            }
+            remove
+            {
+                Console.WriteLine("Delegate removed");
+                frmula -= value;
+            }
+        }
+
+        public double GetProduct(double Prduct1, double Prduct2)
+        {
+            return Prduct1 * Prduct2;
+        }
+
+        public double GetQuitient(double Quotient1, double Quotient2)
+        {
+            return Quotient1 / Quotient2;
+        }
+    }
+}
---
+++ CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{   
+    public partial class FrmCalculator : Form
+    {
+
+        private CalculatorClass cal;
+        private double num1;
+        private double num2; 
+        
+        public FrmCalculator()
+        {
+            InitializeComponent();
+            
+            cbOperator.Items.Add("+");
+            cbOperator.Items.Add("-");
+            cbOperator.Items.Add("*");
+            cbOperator.Items.Add("/");
+
+            cal = new CalculatorClass();
+        }
+
+        private void cbOperator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedOperator = cbOperator.SelectedItem.ToString();
+
+            double result = 0.0;
+
+            switch (cbOperator)
+            {
+                case "+":
+                    cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                    result = cal.GetSum(num1, num2);
+                    break;
+            }
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            cal = new CalculatorClass();
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+
+        }
+    }
+}
